f3007ea6a5dc964ff445185af47694501a4b5bc8
Kanishk Sharma <69013882+kanishk014@users.noreply.github.com>
2021-01-16 13:35:30 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,92 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-sambookas-entry",
+      "name": "Sambookas Entry",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 2.1011663966370824,
+        "pitch": 0.44158460548171874,
+        "fov": 1.3413481656778479
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-sambookas",
+      "name": "Sambookas",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -1.4987426539723483,
+        "pitch": 0.14662625723109457,
+        "fov": 1.3413481656778479
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-sambookas",
+      "name": "Sambookas",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.8245926481239287,
+        "pitch": 0.03515810967890687,
+        "fov": 1.3413481656778479
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
